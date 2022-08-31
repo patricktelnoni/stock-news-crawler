dddfe4a71f873d8adc222262f35b18c8739b1785
Patrick Telnoni <ptelnoni89@gmail.com>
2022-08-31 14:00:12 +0700

    finished scrapping using pagination

--- stockcrawler/spiders/cnbc_spider.py
+++ stockcrawler/spiders/cnbc_spider.py
@@ -1,3 +1,4 @@
+from urllib import request
 import scrapy, logging, csv
 from scrapy import Request
 
@@ -5,31 +6,38 @@ logger = logging.getLogger('logger_cnbc')
 
 class CnbcSpider(scrapy.Spider):
     name            = "cnbc"   
-    # textResult      = []
     def start_requests(self):
         link = 'https://www.cnbcindonesia.com/tag/antm'
         yield scrapy.Request(url = link, callback=self.parse)
 
 
     def parse(self, response):
+        paging      = response.xpath('//div[contains(@class, "paging")]')
+        max_page    = 1
+        if paging.get() is not None:
+            max_page = max([page_number for page_number in paging.xpath('//a[@class=""]/text()').getall() if page_number.isnumeric()])
+            
+        for page_number in range(1, int(max_page)):
+            page_url = 'https://www.cnbcindonesia.com/tag/antm/{}'.format(page_number)
+            yield Request(page_url, callback=self.parsePage)
+            
+    def parsePage(self, response):
         for url in response.selector.xpath('//ul[@class="list media_rows middle thumb terbaru gtm_indeks_feed"]/li/article/a').xpath('@href').getall():
             yield Request(url, callback=self.parseContent)
-
-
+    
     def parseNextPage(self, response):
         data        = response.meta.get('data')
         divContent  = response.xpath('//div[@class="detail_text"]')
         textContent = divContent.xpath('p/span/text()') if divContent.xpath('p/span').get() is not None else divContent.xpath('p/text()')
         for p in textContent.getall():
             data.append(p)
-        print(data)
+       # print(data)
 
     def parseContent(self, response):
         textData    = []
         dateTime    = response.css('.date::text').get()
         title       = response.xpath('//h1/text()').get()
         divContent  = response.xpath('//div[@class="detail_text"]')
-
         textContent = divContent.xpath('p/span/text()') if divContent.xpath('p/span').get() is not None else divContent.xpath('p/text()')
 
         for p in textContent.getall():
