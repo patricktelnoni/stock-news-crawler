6a67348ed365bb1b39634da02b5fc04daad132a4
Patrick Telnoni <ptelnoni89@gmail.com>
2022-09-07 18:39:11 +0700

    successfully acquire access_token through scrapy request and stockbit's api

--- stockcrawler/spiders/stockbit_spider.py
+++ stockcrawler/spiders/stockbit_spider.py
@@ -1,13 +1,31 @@
-import scrapy
+import scrapy, json
+from scrapy.http.request.json_request import JsonRequest
+
 
 class StockbitSpider(scrapy.Spider):
     name = "stockbit"
-
+    # start_urls = ['https://stockbit.com/login']
+    
     def start_requests(self):
+        # login = 'https://stockbit.com/login'
+        login = 'https://stockbit.com/api/login/email'
         link = 'https://stockbit.com/#/symbol/ANTM/keystats'
-        yield scrapy.Request(url = link, callback=self.parse)
-
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Cookie': 'isLoggedIn=true'
+            }
+   
+        yield scrapy.Request(url = login, method='POST', headers=headers, body='username=&password=', callback=self.parse)
+    
     def parse(self, response):
-        print(response)
+       data         = json.loads(response.text)
+       access_token = data['data']['access_token']
+       header       = {'Authorization': 'Bearer {}'.format(access_token)}
+       url          = 'https://exodus.stockbit.com/seasonality/ANTM?year=2022&back_year=10'    
+       yield scrapy.Request(url = url, headers=header, method='GET', callback=self.parse_seasonality)
+        
+    def parse_seasonality(self, response):
+        pass
 
+    
         
