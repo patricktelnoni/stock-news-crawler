ca91d78928e16903c9a129cdf9ddd6ddf50021ba
Patrick Telnoni <ptelnoni89@gmail.com>
2022-09-07 10:57:10 +0700

    fix no .py extension in stockbit spider filename

--- stockcrawler/spiders/stockbit_spider.py
+++ stockcrawler/spiders/stockbit_spider.py
@@ -4,8 +4,10 @@ class StockbitSpider(scrapy.Spider):
     name = "stockbit"
 
     def start_requests(self):
-        link = 'https://stockbit.com/#/symbol/ANTM/'
+        link = 'https://stockbit.com/#/symbol/ANTM/keystats'
         yield scrapy.Request(url = link, callback=self.parse)
 
     def parse(self, response):
         print(response)
+
+        
