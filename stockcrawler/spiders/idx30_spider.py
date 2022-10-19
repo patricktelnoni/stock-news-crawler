19a57df0accca040520a5db9b35c057565806993
Patrick Telnoni <ptelnoni89@gmail.com>
2022-10-19 11:30:04 +0700

    change link to idxchannel.com

--- stockcrawler/spiders/idx30_spider.py
+++ stockcrawler/spiders/idx30_spider.py
@@ -4,20 +4,29 @@ class Idx30Spider(scrapy.Spider):
     name = "idx30"
 
     def start_requests(self):
-        link = 'https://www.kontan.co.id/indeks-idx30'
+        # link = 'https://www.kontan.co.id/indeks-idx30'
+        # link = 'https://www.infovesta.com/index/data_info/saham/idx30'
+        # link = 'https://www.infojabodetabek.com/daftar-saham-indeks-idx30/'
+        link = 'https://www.idxchannel.com/market-news/simak-daftar-saham-idx30-periode-agustus-2022-januari-2023-ada-3-saham-baru'
         yield scrapy.Request(url = link, callback=self.parse)
 
     def parse(self, response):
-        #idx30_stock = {"code":[]}
+        print(response)
+        # idx30_stock = {"code":[]}
         result      = []
-        content     = response.xpath('//table[@class="table table-bordered table-hover table-condensed"]/tbody')
-        for index, data in enumerate(content.xpath('tr/td[2]/text()').getall()):
-            if index < 30:
-                result.append(data)
+        # content         = response.xpath('//table[@class="table table-bordered table-striped"]/tbody')
+        # content         = response.xpath('//*[@class="wp-block-table"]/').getall()
         
-        # idx30_stock["code"] = result
-        yield {
-            "stocks" : result
-        }
+        #content     = div.xpath('//table[class="table table-bordered table-striped"]/tbody')
+        #content     = response.xpath('//div[@class="panel panel-default"]/table[@class="table table-bordered table-striped"]/tbody/tr/td[1]/text()').getall()
+        content     = response.xpath('//div[@class="text-body--3"]/table/tr/td[3]/text()').getall()
+        print(content)
+        for index, data in enumerate(content):
+            print(data)
+            result.append(data)
+        
+        
+        # print(idx30_stock)
+        yield {"code" : result}
 
         
