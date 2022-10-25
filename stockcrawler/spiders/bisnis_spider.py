f9236727d62a282f8feb5e6ea1025908389ef052
Patrick Telnoni <ptelnoni89@gmail.com>
2022-10-25 16:38:33 +0700

    fix start_requests method

--- stockcrawler/spiders/bisnis_spider.py
+++ stockcrawler/spiders/bisnis_spider.py
@@ -3,20 +3,22 @@ import scrapy, re
 class BisnisSpider(scrapy.Spider):
     name = "bisnis"
 
-    def start_request(self):
-        stock_list  = ['ADRO', 'ANTM', 'ARTO', 'ASII', 'BBCA', 'BBNI', 'BBRI', 'BMRI', 'BRPT', 'BUKA', 'CPIN', 'EMTK', 'GOTO', 'HRUM', 'ICBP', 'INCO', 'INDF', 'INKP', 'ITMG', 'KLBF', 'MDKA', 'PGAS', 'PTBA', 'SMGR', 'TBIG', 'TINS', 'TLKM', 'TOWR', 'UNTR', 'UNVR']
+    def start_requests(self):
+        stock_list  = ['ADRO', 'ANTM', 'ARTO', 'ASII', 'BBCA', 'BBNI', 'BBRI', 'BMRI', 'BRPT', 'BUKA', 'CPIN', 'EMTK', 'GOTO', 'HRUM', 'ICBP', 'INCO', 'INDF', 'INKP', 'ITMG', 
+                        'KLBF', 'MDKA', 'PGAS', 'PTBA', 'SMGR', 'TBIG', 'TINS', 'TLKM', 'TOWR', 'UNTR', 'UNVR']
         
         for stock in stock_list:
-            link = 'https://search.bisnis.com/?q={}'.format(stock)
+            link    = 'https://search.bisnis.com/?q={}'.format(stock.lower())
             yield scrapy.Request(url = link, callback=self.parse, meta={'kode_saham' : stock.lower()})
 
     def parse(self, response):
         kode_saham       = response.meta['kode_saham']
         pagination       = response.xpath('//ul[contains(class, "pages")]')
         max_page         = 1
-        print(kode_saham)
+
         if pagination is not None:
             last_link       = response.xpath('//ul[@class="pages"]/li/a/@href').getall()[-1]
             query_string    = '(?<=per_page=)([^&]*)(?=&)?'
             max_page        = re.findall(query_string, last_link)
+            print(max_page[0])
             
