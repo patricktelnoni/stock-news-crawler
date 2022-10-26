aedffe35e4f1e76eab93c73ad8f17d70482c6a99
Patrick Telnoni <ptelnoni89@gmail.com>
2022-10-26 10:45:02 +0700

    loop pagiantion and parse content from bisnis.com

--- stockcrawler/spiders/bisnis_spider.py
+++ stockcrawler/spiders/bisnis_spider.py
@@ -1,3 +1,4 @@
+from curses import meta
 import scrapy, re
 
 class BisnisSpider(scrapy.Spider):
@@ -20,5 +21,30 @@ class BisnisSpider(scrapy.Spider):
             last_link       = response.xpath('//ul[@class="pages"]/li/a/@href').getall()[-1]
             query_string    = '(?<=per_page=)([^&]*)(?=&)?'
             max_page        = re.findall(query_string, last_link)
-            print(max_page[0])
+        
+        for i in range(1, int(max_page[0])):
+            link = 'https://search.bisnis.com/?q={}&per_page={}'.format(kode_saham, i)
+            yield scrapy.Request(url=link, callback=self.iterate_page, meta={'kode_saham': kode_saham})
+            
+
+    def iterate_page(self, response):
+        kode_saham       = response.meta['kode_saham']
+        list_link = response.xpath('//ul[@class="list-news"]/li/div[@class="col-sm-9"]/h2/a/@href').getall()
+        for link in list_link:
+            yield scrapy.Request(url=link, call_back=self.parse_content, meta={'kode_saham': kode_saham})
+
+    def parse_content(self, response):
+        kode_saham       = response.meta['kode_saham']
+
+        title   = response.xpath('/h1[@class="title-only]/text()').extract_first()
+        content = response.xpath('//div[@class="col-sm-10 col-sm-offset-2"]/p/text()').getall()
+        date    = response.xpath('//div[@class="news-description"]/span//text()').get()
+        
+        yield{
+            'saham'             : kode_saham,
+            'tanggal_berita'    : date,
+            'judul'             : title,
+            'isi'               : ' '.join(content)
+        }
+
             
